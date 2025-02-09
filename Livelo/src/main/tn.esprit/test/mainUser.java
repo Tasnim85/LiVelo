cdfca9a69470d6a6e0fc13e3f6222e2b8b1b80c6
tasnim <tasnimbenhassine1@gmail.com>
2025-02-09 14:49:07 +0100

    yarabi le

--- Livelo/src/main/tn.esprit/test/mainUser.java
+++ Livelo/src/main/tn.esprit/test/mainUser.java
@@ -1,32 +1,32 @@
-    package test;
+package test;
 
-    import models.User;
-    import models.role_user;
-    import models.type_vehicule;
-    import services.CrudUser;
+import models.User;
+import models.role_user;
+import models.type_vehicule;
+import services.CrudUser;
 
-    public class mainUser {
-        public static void main(String[] args) {
-            User user = new User(
-                    "Benhassine",
-                    "Tasnim",
-                    role_user.delivery_person,
-                    true,
-                    "tunis",
-                    type_vehicule.e_bike,
-                    "tasnim.benhassine@esprit.tn",
-                    "123",
-                    "28740885",
-                    "11673727"
-            );
-            CrudUser crudUser = new CrudUser();
-            crudUser.delete(18);
-            crudUser.add(user);
-            crudUser.getAll();
+public class mainUser {
+    public static void main(String[] args) {
+        User user = new User(
+                "Benhassine",
+                "Tasnim",
+                role_user.delivery_person,
+                true,
+                "tunis",
+                type_vehicule.e_bike,
+                "tasnim.benhassine@esprit.tn",
+                "123",
+                "28740885",
+                "11673727"
+        );
+        CrudUser crudUser = new CrudUser();
+        crudUser.delete(18);
+        crudUser.add(user);
+        crudUser.getAll();
 
-            user.setAdresse("Sousse");
-            user.setEmail("tasnim.sousse@esprit.tn");
-            crudUser.update(user);
-            crudUser.getAll();
-        }
+        user.setAdresse("Sousse");
+        user.setEmail("tasnim.sousse@esprit.tn");
+        crudUser.update(user);
+        crudUser.getAll();
     }
+}
